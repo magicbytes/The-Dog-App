a76005400f2db09aa729996b3b5458bd3db3fd83
Alex <cebotari.alex@gmail.com>
2022-05-30 14:12:04 +0100

    chore: update dependencies

--- app/src/main/java/com/magicbytes/thedogapp_cv/feat/dogs/data/DogsNetworkDataSource.kt
+++ app/src/main/java/com/magicbytes/thedogapp_cv/feat/dogs/data/DogsNetworkDataSource.kt
@@ -2,21 +2,21 @@ package com.magicbytes.thedogapp_cv.feat.dogs.data
 
 import com.magicbytes.thedogapp_cv.api.DogsService
 import com.magicbytes.thedogapp_cv.api.data.DogBreed
-import com.magicbytes.thedogapp_cv.utility.ServiceResponse
 import timber.log.Timber
 import javax.inject.Inject
 
-class DogsNetworkDataSource @Inject constructor(private val dogsService: DogsService) : DogsRepository.DataSource {
+class DogsNetworkDataSource @Inject constructor(private val dogsService: DogsService) :
+    DogsRepository.DataSource {
 
-    override suspend fun loadAllBreeds(): ServiceResponse<List<DogBreed>> {
+    override suspend fun loadAllBreeds(): Result<List<DogBreed>> {
         val response = dogsService.getAllBreeds()
 
         return if (response.isSuccessful) {
-            ServiceResponse.Success(response.body().orEmpty())
+            Result.success(response.body().orEmpty())
         } else {
-            val errorMessage = response.errorBody()?.string()
+            val errorMessage = response.errorBody()?.string().orEmpty()
             Timber.e("There was an issue when loading all breeds: $errorMessage")
-            ServiceResponse.Error()
+            Result.failure(Throwable(errorMessage))
         }
     }
 }
--- app/src/main/java/com/magicbytes/thedogapp_cv/feat/dogs/data/DogsRepository.kt
+++ app/src/main/java/com/magicbytes/thedogapp_cv/feat/dogs/data/DogsRepository.kt
@@ -1,14 +1,13 @@
 package com.magicbytes.thedogapp_cv.feat.dogs.data
 
 import com.magicbytes.thedogapp_cv.api.data.DogBreed
-import com.magicbytes.thedogapp_cv.utility.ServiceResponse
 import javax.inject.Inject
 
 class DogsRepository @Inject constructor(private val dataSource: DataSource) {
 
     private var allBreedsCache: List<DogBreed> = emptyList()
 
-    suspend fun loadAllBreeds(): ServiceResponse<List<DogBreed>> {
+    suspend fun loadAllBreeds(): Result<List<DogBreed>> {
         allBreedsCache = emptyList()
 
         val response = dataSource.loadAllBreeds()
@@ -22,6 +21,6 @@ class DogsRepository @Inject constructor(private val dataSource: DataSource) {
     }
 
     interface DataSource {
-        suspend fun loadAllBreeds(): ServiceResponse<List<DogBreed>>
+        suspend fun loadAllBreeds(): Result<List<DogBreed>>
     }
 }
